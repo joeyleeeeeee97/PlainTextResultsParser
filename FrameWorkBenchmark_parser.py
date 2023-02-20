e743aeb5d8a2132d043e5c5d71dfcd2f2813543a
joeylee <30545334+joeyleeeeeee97@users.noreply.github.com>
2023-02-20 14:09:14 +0800

    Update FrameWorkBenchmark_parser.py

--- FrameWorkBenchmark_parser.py
+++ FrameWorkBenchmark_parser.py
@@ -1,11 +1,10 @@
-# -*- coding: UTF-8 -*-
 import json as js
-import os, getopt, argparse, collections
+import argparse
+import collections
 from prettytable import PrettyTable
 
 workloads = ["fortune", "plaintext", "db", "update", "json", "query"]
 stress = ["concurrencyLevels", "queryIntervals", "pipelineConcurrencyLevels"]
-result_dict = collections.defaultdict(dict)
 workload_field_map = {"query": "queryIntervals", "plaintext": "pipelineConcurrencyLevels", "db": "concurrencyLevels",
                       "update": "concurrencyLevels", "fortune": "concurrencyLevels", "json": "concurrencyLevels"}
 
@@ -24,10 +23,12 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+result_dict = collections.defaultdict(dict)
+
 # gather data
-for json_results in [js.loads(open(f).read()) for f in args.files]:
+for json_results in map(lambda f: js.loads(open(f).read()), args.files):
     stress_field_map = dict([(s, json_results[s]) for s in stress])
-    for workload in [w for w in workloads if json_results["rawData"][w]]:
+    for workload in filter(lambda w: json_results["rawData"][w], workloads):
         for test_name, test_result in json_results["rawData"][workload].items():
             result_dict[workload][test_name] = test_result
 
@@ -37,7 +38,11 @@ for interested_data in args.datas:
         pt = PrettyTable()
         pt.title = "Type: " + k + ", Result: " + interested_data
         pt.field_names = [workload_field_map[k]] + list(result_dict[k].keys())
-        for i in range(len(workload_results[next(iter(workload_results))])):
-            data_row = [v[i].get(interested_data) for v in workload_results.values()]
-            pt.add_row([stress_field_map[workload_field_map[k]][i]] + data_row)
+        pt.add_rows([
+            [
+                stress_field_map[workload_field_map[k]][i],
+                *[v[i].get(interested_data) for v in workload_results.values()]
+            ]
+            for i in range(len(workload_results[next(iter(workload_results))]))
+        ])
         print(pt)
